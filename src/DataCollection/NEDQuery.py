239c8a45ecf419a638841be59984718e9e6cfcba
Mo59471 <9674025@graniteschools.org>
2026-07-07 23:08:29 -0600

    Update NEDQuery.py

--- src/DataCollection/NEDQuery.py
+++ src/DataCollection/NEDQuery.py
@@ -4,28 +4,29 @@ import time
 objID = []
 redshifts = []
 zOG = []
+toggle = 0
 
-with open("WISEDataOnly.txt", "r") as file:
-    lines = file.readlines()
-    for i in range(1, len(lines)):
-        objID.append(lines[i][8:27])
-        zOG.append(lines[i][28:33])
+if toggle == 0:
+    with open("WISEDataOnly.txt", "r") as file:
+        lines = file.readlines()
+        for i in range(1, len(lines)):
+            objID.append(lines[i][8:27])
+            zOG.append(lines[i][28:33])
         
-for j in range(0, len(objID)):
-    objID[j]="SDSS " + objID[j].strip()
+    for j in range(0, len(objID)):
+        objID[j]="SDSS " + objID[j].strip()
 
-for obj in objID:
-    try:
-        result = Ned.query_object(obj)
-        rshft = result["Redshift"][0]
-        redshifts.append(rshft)
-        time.sleep(1)
-    except:
-        redshifts.append(zOG[objID.index(obj)])
+    for obj in objID:
+        try:
+            result = Ned.query_object(obj)
+            rshft = result["Redshift"][0]
+            redshifts.append(rshft)
+            time.sleep(1)
+        except:
+            redshifts.append(zOG[objID.index(obj)])
 
-for z in range(len(redshifts)):
-    redshifts[z] = f"{redshifts[z]}\n"
+    for z in range(len(redshifts)):
+        redshifts[z] = f"{redshifts[z]}\n"
 
-with open("Redshifts.txt", "w") as file:
-    file.writelines(redshifts)
-
+    with open("WISERedshifts.txt", "w") as file:
+        file.writelines(redshifts)
