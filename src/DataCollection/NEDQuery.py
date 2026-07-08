bf511c9818e780a8d7c785fe6b6df2d528129cde
Mo59471 <9674025@graniteschools.org>
2026-07-08 00:01:11 -0600

    Update NEDQuery.py

--- src/DataCollection/NEDQuery.py
+++ src/DataCollection/NEDQuery.py
@@ -4,7 +4,10 @@ import time
 objID = []
 redshifts = []
 zOG = []
-toggle = 0
+RA = []
+Dec = []
+data = []
+toggle = 1
 
 if toggle == 0:
     with open("WISEDataOnly.txt", "r") as file:
@@ -30,3 +33,26 @@ if toggle == 0:
 
     with open("WISERedshifts.txt", "w") as file:
         file.writelines(redshifts)
+
+else:
+    objID = ["Mrk 335","Mrk 590","IRAS 03450+0055","Akn 120","MCG+08-11-011","Mrk 79","Mrk 110","NGC 3227","NGC 3516","Mrk 744","NGC 4051","NGC 4151","NGC 4593","NGC 5548","Mrk 817","Mrk 509","NGC 7469"]
+    for obj in objID:
+        try:
+            result = Ned.query_object(obj)
+            rshft = result["Redshift"][0]
+            ra = result["RA(deg)"][0]
+            dec = result["DEC(deg)"][0]
+            redshifts.append(rshft)
+            RA.append(ra)
+            Dec.append(dec)
+            time.sleep(1)
+        except:
+            redshifts.append("none")
+            RA.append("none")
+            Dec.append("none")
+    
+    for i in range(len(redshifts)):
+        data.append(f"{redshifts[i]}|{RA[i]}|{Dec[i]}\n")
+    
+    with open("KoshidaData.txt", "w") as file:
+        file.writelines(data)
